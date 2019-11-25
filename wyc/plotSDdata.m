6d9c7594c07a4392733537610e7f8201622341d6
unknown <13297989803wyc>
2019-11-25 14:25:27 +0800

    first commit

---
+++ wyc/plotSDdata.m
@@ -0,0 +1,5 @@
+cd F://;
+mat_end = importfile("FINALM.TXT");
+mesh(-mat_end);
+%delete("*.TXT");
+cd D:\Users\matlab;
---
+++ wyc/readme.txt
@@ -0,0 +1,9 @@
+使用Vivado 2019.1版本运行代码udp_test_v4.7_Verilog
+
+
+matlab使用2018a运行plotSDdata.m
+
+
+
+
+作者；街道口最强
